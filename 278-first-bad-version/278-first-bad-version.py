8a95ce4cb55bfd302c8a7e30cc9310b8834f16e9
Kunal batham <89760031+KunalBatham2701@users.noreply.github.com>
2022-09-01 14:15:11 +0530

    Time: 57 ms (20.07%), Space: 13.8 MB (62.19%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstBadVersion(self, n) -> int:
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
