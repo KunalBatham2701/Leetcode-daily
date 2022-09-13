b02eebd7396ca80bf7903d51130a62061fb91f2f
Kunal batham <89760031+KunalBatham2701@users.noreply.github.com>
2022-09-13 23:41:17 +0530

    Time: 6 ms (34.71%), Space: 47.1 MB (83.16%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+         int cnt = 0;
+        for(int d : data){
+            if(cnt == 0){
+                if((d>>5) == 0b110) cnt = 1;
+               else if((d>>4) == 0b1110) cnt=2;
+               else if((d>>3) == 0b11110) cnt=3;
+               else if((d>>7) != 0) return false;
+            }else{
+                if((d>>6) != 0b10) return false;
+                cnt--;
+            }
+        }
+        return cnt == 0;
+    }
+}
