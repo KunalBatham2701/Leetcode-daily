58d31aacec0f0bbda7dc821323f069df8d90f1db
Kunal batham <89760031+KunalBatham2701@users.noreply.github.com>
2023-04-26 00:00:14 +0530

    Added solution - LeetHub

---
+++ Game Of Subsets - GFG/game-of-subsets.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    void precom(vector<int>&v,vector<int>prime){
+        for(int i=2;i<=30;i++){
+            if((i%4)&&(i%9)&&(i%25)){
+                int mask=0;
+                for(int j=0;j<10;j++)if(!(i%prime[j]))mask|=(1<<j);
+                v[i]=mask;
+            }
+        }
+    }
+    int goodSubsets(vector<int> &arr, int n){
+        vector<int>v(31,0);
+        vector<int>prime={2,3,5,7,11,13,17,19,23,29};
+        precom(v,prime);
+        const int mod=1e9+7;
+        map<int,int>mp;
+        long long ans=0;
+        vector<long long>dp((1<<10)+1,0);
+        int onecnt=0;
+        for(auto i:arr){
+            if(i==1)onecnt++;
+            else mp[i]++;
+        }
+        dp[0]=1;
+        //counting
+        for(auto i:mp){
+            if(v[i.first]){
+                for(int j=0;j<(1<<10);j++){
+                    if((v[i.first]&j)==0){
+                        dp[j|v[i.first]]+=dp[j]*i.second;
+                        dp[j|v[i.first]]%=mod;
+                    }
+                }
+            }
+        }
+        for(auto i:dp)ans+=i,ans%=mod;
+        ans--;
+        if(onecnt){
+            while(onecnt--){
+                ans*=2;
+                ans%=mod;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        Solution ob;
+        cout<<ob.goodSubsets(a, n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
